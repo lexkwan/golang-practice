2c401465328ea86c0549c196f1e5c3a144284bc5
lexkwan <lexkwan@gmail.com>
2019-06-11 23:35:20 +0800

    aadd go-kit-exmp

---
+++ go-kit-Exmp/notificator/cmd/main.go
@@ -0,0 +1,7 @@
+package main
+
+import service "github.com/lexkwan/golang-practice/go-kit-Exmp/notificator/cmd/service"
+
+func main() {
+	service.Run()
+}
---
+++ go-kit-Exmp/users/cmd/main.go
@@ -0,0 +1,7 @@
+package main
+
+import service "github.com/lexkwan/golang-practice/go-kit-Exmp/users/cmd/service"
+
+func main() {
+	service.Run()
+}
